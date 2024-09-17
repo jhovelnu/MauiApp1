68d163ae1f6f45284aa9d55897576cd1101e6696
Jonathan Velandia <jonathan.velandia@devinmotion.co>
2024-09-17 17:41:10 -0500

    adjust manifest and MainActivity

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.AppCompat.App;
 
 namespace MauiApp1
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightNo;
+
+            RequestedOrientation = ScreenOrientation.Portrait;
+
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
